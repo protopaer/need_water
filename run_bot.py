77d2a53c7443a71fa435f99ce777e466d3c729dc
Maxim Salnikov <maximsalnikov174@icloud.com>
2025-03-26 12:20:42 +0500

    очищаю базу

--- run_bot.py
+++ run_bot.py
@@ -9,7 +9,7 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 from apscheduler.triggers.cron import CronTrigger
-from sqlalchemy import insert, update
+from sqlalchemy import delete, insert, update
 
 from bot_logging import configure_logging
 from constants import (FIRST_SECTION,
@@ -355,6 +355,20 @@ async def setup_bot(token: str) -> Bot:
     return bot
 
 
+async def clear_table(engine, model):
+    """Асинхронная очистка указанной таблицы"""
+    async with AsyncSessionLocal() as session:
+        try:
+            # Для SQLAlchemy 2.0+
+            await session.execute(delete(model))
+            await session.commit()
+            logging.info(f"Таблица {model.__tablename__} успешно очищена")
+        except Exception as e:
+            await session.rollback()
+            logging.error(f"Ошибка при очистке таблицы {model.__tablename__}: {e}")
+            raise
+
+
 # Run the bot
 async def main() -> None:
 
@@ -364,6 +378,10 @@ async def main() -> None:
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
         logging.info('Движок создан, база подключена')
+
+    # Очищаем таблицы
+    await clear_table(engine, TgAccounts)
+    await clear_table(engine, Order)
 
     token = str(os.getenv('bot_token'))
     bot = await setup_bot(token)
