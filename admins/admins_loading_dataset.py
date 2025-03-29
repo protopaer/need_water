5494ec2821f9a06af073410d140abac2a89ad6b3
Maxim Salnikov <maximsalnikov174@icloud.com>
2025-03-29 23:54:41 +0500

    исправил ошибки

--- admins/admins_loading_dataset.py
+++ admins/admins_loading_dataset.py
@@ -23,28 +23,29 @@ def return_build(data) -> Builds:
     )
 
 
-async def load_object_in_db(object):
+async def load_object_in_db(object, dataname):
     """
     Загрузка объекта в базу данных.
     """
     async with AsyncSessionLocal() as session:
         try:
-            if isinstance(object, Offices):
+            if dataname == 'office':
                 object = return_office(object)
-                text_for_log = ('Кабинета №', 'office_number')
-            elif isinstance(object, Builds):
+                text_for_log = 'Кабинета'
+            elif dataname == 'build':
                 object = return_build(object)
-                text_for_log = ('Здания', 'name')
+                text_for_log = 'Здания'
             else:
                 logging.error('Неизвестный объект')
+                text_for_log = 'чего-то еще!'
 
             session.add(object)
             await session.commit()
         except Exception as e:
             await session.rollback()
-            obj_name, elem_in_obj = text_for_log
+            obj_id = object.get('id')
             logging.error(
-                f'Проблема с загрузкой {obj_name}{object[{elem_in_obj}]}.'
+                f'Проблема с загрузкой {text_for_log}:{obj_id}.'
                 f'Ошибка: {e}',
                 exc_info=True
             )
@@ -61,7 +62,7 @@ async def upload_dataset(dataname):
     ) as file:
         csv_reader = csv.DictReader(file)
         for row in csv_reader:
-            await load_object_in_db(row)
+            await load_object_in_db(row, dataname)
 
 
 async def all_upload():
